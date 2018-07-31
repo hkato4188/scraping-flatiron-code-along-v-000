d7c7247c6665283428302deea9ecdcacf1963fc6
Hiroki Kato <hkato@sas.upenn.edu>
2018-07-31 20:35:43 +0000

    automatically backed up by learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,17 +1,37 @@
 class Course
-  attr_accessor :title, :schedule, :description
-  @@all = []
 
-  def initialize
-    @@all << self
-  end
+  attr_accessor :title, :schedule, :description
+
+  # def initialize(title = nil, schedule = nil, description = nil)
+  #   @title = title
+  #   @schedule = schedule
+  #   @description = description
+  #   self.all << self
+  # end
 
   def self.all
-    @@all
-  end
-
-  def self.reset_all
-    @@all.clear
+    @@all = Array.new
   end
 
 end
+
+
+
+
+# class Course
+#   attr_accessor :title, :schedule, :description
+#   @@all = []
+#
+#   def initialize
+#     @@all << self
+#   end
+#
+#   def self.all
+#     @@all
+#   end
+#
+#   def self.reset_all
+#     @@all.clear
+#   end
+#
+# end
